2d66dba09106ff57374ad00ae260fee7069e11fb
ratna <116795648+ratnababu7571@users.noreply.github.com>
2024-07-20 14:47:17 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "ratnadevops"
     key            = "my-terraform-environment/main"
-    region         = "us-east-1"
-    dynamodb_table = "my-dynamo-db-table"
+    region         = "ap-south-1"
+    dynamodb_table = "mycloud-dynamo-db-table"
   }
 }
